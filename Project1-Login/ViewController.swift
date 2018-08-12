dfc49e26f1fccfe027c53dced634e423df4d12eb
Xuân Quỳnh <quynhlx@itisol.vn>
2018-08-12 21:35:51 +0700

    Initial Commit

---
+++ Project1-Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1-Login
+//
+//  Created by Babie Xcode on 8/12/18.
+//  Copyright © 2018 Babie Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
